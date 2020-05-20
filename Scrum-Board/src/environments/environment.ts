1ec711f6c5d57d14f0e691461b253402efd2cb15
Masud Rasa <m.rasa1@student.avans.nl>
2020-05-20 20:07:30 +0200

    app module change and firebase environment credentials
    Co-Authored-By: Darjush Kolahi <darjushkolahi@users.noreply.github.com>

--- Scrum-Board/src/environments/environment.ts
+++ Scrum-Board/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA325XVcisUWQ8z7dor7SwEx9KYevq82zU",
+    authDomain: "scrum-board-f8bf4.firebaseapp.com",
+    databaseURL: "https://scrum-board-f8bf4.firebaseio.com",
+    projectId: "scrum-board-f8bf4",
+    storageBucket: "scrum-board-f8bf4.appspot.com",
+    messagingSenderId: "369214962244",
+    appId: "1:369214962244:web:97fe1c7075161db89ada11"
+  }
 };
 
 /*
