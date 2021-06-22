ea2a47ea099a3e02428da019b26785efe1786055
Mas Rasa <m.rasa1@student.avans.nl>
2021-06-22 16:40:12 +0200

    add environment
    Co-Authored-By: Darjush Kolahi <31481455+DarjushKolahi@users.noreply.github.com>

--- Scrum-Board/src/environments/environment.prod.ts
+++ Scrum-Board/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA325XVcisUWQ8z7dor7SwEx9KYevq82zU",
+    authDomain: "scrum-board-f8bf4.firebaseapp.com",
+    databaseURL: "https://scrum-board-f8bf4.firebaseio.com",
+    projectId: "scrum-board-f8bf4",
+    storageBucket: "scrum-board-f8bf4.appspot.com",
+    messagingSenderId: "369214962244",
+    appId: "1:369214962244:web:97fe1c7075161db89ada11"
+  }
 };
